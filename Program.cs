f795c6cbb733104277a07d5d938013613c327f72
Sergey Protopopov <protopopov_s@mail.ru>
2024-02-17 18:07:34 +0300

    Написаны методы генерации и печати случайного массива строк

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string[] MakeStringsArray(int amount_of_strings)
+{
+    string abc = "abcdefghijklmnopqrstuvwxyz";
+    string[] array = new string[amount_of_strings];
+    for (int i = 0; i < amount_of_strings; i++)
+    {
+        string rand_str = "";
+        for (int j = 0; j < new Random().Next(1, 20); j++)
+        {
+            rand_str += abc[new Random().Next(0, 26)];
+        }
+        array[i] = rand_str;
+    }
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"'{array[i]}'\t");
+    }
+}
+
+Console.Write("Введите количество элементов в массиве строк: ");
+int amount_of_strings = Convert.ToInt32(Console.ReadLine());
+string[] array = MakeStringsArray(amount_of_strings);
+PrintArray(array);
